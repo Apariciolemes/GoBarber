36e173c5011b133d5e06411e89ad2410b9e46575
Aparicio Lemes Neto <55216232+Apariciolemes@users.noreply.github.com>
2021-04-23 14:59:10 -0300

    feat(appointment): migrations

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,10 +1,13 @@
-import { parse, v4 as uuid } from 'uuid';
-
+import { parse, v4 as uuid } from 'uuid'
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+@Entity('appointments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
-
+    @Column('time without time zone')
     date: Date;
 
     constructor({ provider, date }: Omit<Appointment, 'id'>) {
